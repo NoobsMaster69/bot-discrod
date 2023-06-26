dcfc86963874f5ab1344da86cf061724876ffd74
NoobsMaster69 <75945332+NoobsMaster69@users.noreply.github.com>
2023-06-26 21:43:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "1122881470810304645", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTEyMjg4MTQ3MDgxMDMwNDY0NQ.GFemQD.3cPiycru58ITkk43rR268WJ18r-i5aMgvJOhMs", //- Bot's Token
+	clientId: process.env.clientId || "1122881470810304645", //- ID of the bot
+	clientSecret: process.env.clientSecret || "kCu3pCUlzOgBznoRYvlhBoGXt76hquKz", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			host: "lavalink-replit.noobsmaster69.repl.co", //- The host name or IP of the lavalink server.
+			port: 443, // The port that lavalink is listening to. This must be a number!
+			password: "maybeiwasboring", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
